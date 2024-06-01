3805a2efca16f61ab593537a7988b03933215c84
JingHuangSu1996 <jing.tech.tw@gmail.com>
2024-06-01 11:53:32 +0800

    chore: build failed

--- components/lab/ui/select.tsx
+++ components/lab/ui/select.tsx
@@ -14,7 +14,7 @@ const SelectValue = SelectPrimitive.Value;
 
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> & { showIcon?: boolean }
 >(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
