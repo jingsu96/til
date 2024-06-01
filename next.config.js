bf4c1f47091ab953a66f2e031b13482ff0748f6f
JingHuangSu1996 <jing.tech.tw@gmail.com>
2024-06-01 18:09:02 +0800

    chore: fix invalid script

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.vercel-scripts.com *.googletagmanager.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.vercel-scripts.com *.googletagmanager.com *.doubleclick.net;
   style-src 'self' giscus.app 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com *.imgur.com;
